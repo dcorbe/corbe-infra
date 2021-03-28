4566fe095ab94c08464a62347cd0e296b32860d8
Daniel Corbe <daniel@corbe.net>
2021-03-28 15:38:41 -0400

    Argo now runs in the default namespace

--- argo-password.sh
+++ argo-password.sh
@@ -1,4 +1,4 @@
-kubectl -n argocd patch secret argocd-secret \
+kubectl patch secret argocd-secret \
   -p '{"stringData": {
     "admin.password": "$2a$10$NdxWXs.Y3hKurUD3WwXTC.GWm6mNSU0FL1kqaGErZLvbh/zTKGhs2",
     "admin.passwordMtime": "'$(date +%FT%T%Z)'"
